21168978ba646f4fd6150842887ddf602998b294
Darius A. Görgen <info@dariusgoergen.com>
2026-02-05 07:41:56 +0100

    adds device management at important locations

--- seapig/scores/knn.py
+++ seapig/scores/knn.py
@@ -202,7 +202,7 @@ class KNNScore(EmbeddingScore, ABC):
         if self.pca is not None:
             X = self.pca.predict(X)
         score = self._distance(query=X)
-        return score
+        return score.to(device=self.threshold.device)
 
     @classmethod
     def _stat(self, x: torch.Tensor, stat: str = "max") -> torch.Tensor:
