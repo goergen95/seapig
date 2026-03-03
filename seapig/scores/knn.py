fix: remove sqrt from euclidean distance calculation

seapig/scores/knn.py
@@ -389,7 +389,7 @@ class EuclideanScore(KNNScore):
     @torch.inference_mode()
     def _distance(self, query: torch.Tensor, kpn: int = 0) -> torch.Tensor:
         """Calculate the KNN distance of a query against a populated index."""
-        return torch.sqrt(self._query_index(query, kpn))
+        return self._query_index(query, kpn)
 
 
 class CosineScore(KNNScore):